d49ba248bcbc3d1a7147b8de545feeab3abe1ccd
cxho <44372250+cxho@users.noreply.github.com>
2018-11-02 08:42:42 -0700

    random guess

--- notes/Lizbeth Perez - Guess Game.py
+++ notes/Lizbeth Perez - Guess Game.py
@@ -1,5 +1,12 @@
 # Random Numbers
 import random
-(random.randint(0, 10))
+a = (random.randint(0, 10))
 random_guess = int(input("Guess a number between 0 and 10."))
-if input < 10
+if random_guess < a:
+    print("The number is lower.")
+    random_guess = int(input("Guess again."))
+elif random_guess > a:
+    print("Your number was too high.")
+    random_guess = int(input("Guess again."))
+elif random_guess == a:
+    print("You guessed right.")
