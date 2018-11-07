6031e3dc4c1f02101030571c0ef2a9b190cb0d24
cxho <44372250+cxho@users.noreply.github.com>
2018-11-07 08:06:58 -0800

    guess game is finished

--- notes/Lizbeth Perez - Guess Game.py
+++ notes/Lizbeth Perez - Guess Game.py
@@ -2,11 +2,15 @@
 import random
 a = (random.randint(0, 10))
 random_guess = int(input("Guess a number between 0 and 10."))
-if random_guess < a:
-    print("The number is lower.")
-    random_guess = int(input("Guess again."))
-elif random_guess > a:
-    print("Your number was too high.")
-    random_guess = int(input("Guess again."))
-elif random_guess == a:
-    print("You guessed right.")
+guesses_left = 5
+win = False
+while guesses_left > 0 and not win:
+    if random_guess < a:
+        print("The number is too low.")
+        random_guess = int(input("Guess again."))
+    elif random_guess > a:
+        print("Your number was too high.")
+        random_guess = int(input("Guess again."))
+    elif random_guess == a:
+        print("You guessed right.")
+        break
