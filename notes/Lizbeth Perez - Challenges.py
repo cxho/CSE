ad02f962815983de8f9da239e0082149521708ad
cxho <44372250+cxho@users.noreply.github.com>
2018-12-06 08:42:08 -0800

    challenges 6, 7, 8, 9

--- notes/Lizbeth Perez - Challenges.py
+++ notes/Lizbeth Perez - Challenges.py
@@ -37,3 +37,44 @@ r = int(input("What is the radius of a circle"))
 pi = 3.14
 area2 = pi*r**2
 print("area", area2)
+
+
+# Challenge 6
+r = int(input("What is the radius of the sphere?"))
+pi = 3.14
+volume = 4/3*pi*r**3
+print("volume", volume)
+
+
+# Challenge 7
+n = int(input("Enter a number."))
+answer = n+n*n+n**3
+print(answer)
+
+
+# Challenge 8
+a = int(input("Enter a number."))
+if a == 2000 - 150 or a == 3000 - 150:
+    print("This number is within 150 of 2000 or 3000.")
+elif a < 2000 - 150 or a < 3000 - 150:
+    print("This number is not within 150 of 2000 and 3000")
+elif a == 2000 + 150 or a == 3000 + 150:
+    print("This number is within 150 of 2000 and 3000")
+elif a > 2000 + 150 or a > 3000 + 150:
+    print("This number is not within 150 of 2000 or 3000")
+elif a == 2000 or 3000:
+    print("This number is within 150 of 3000 or 2000")
+
+
+# Challenge 9
+def is_vowel(char):
+    letters = "aeiou"
+    return char in letters
+
+
+print(is_vowel('a'))
+print(is_vowel('l'))
+print(is_vowel('i'))
+print(is_vowel('o'))
+print(is_vowel('t'))
+
