e6344c59eef54ad2d3caa919e750467228ff40e5
cxho <44372250+cxho@users.noreply.github.com>
2019-01-11 09:43:07 -0800

    random.choice, string

--- notes/Lizbeth Perez - Hangman.py
+++ notes/Lizbeth Perez - Hangman.py
@@ -1,4 +1,12 @@
-word_bank = ["did you eat today?", "Backpack", "clock", "sweaters", "pizza", "i want to go to mars", "tables", "camels",
+import random
+word_bank = ["did you eat today?", "Backpack", "clock", "sweaters", "pizza", "i like turtles", "tables", "camels",
              "Dora the Explorer", "did you do your homework?"]
 guesses_left = 8
 letters_guessed = []
+word = random.choice(word_bank)
+print(word)
+
+string1 = word
+list1 = list(string1)
+print(list1)
+
