c2883fc6e9a078cf495319847c9d930e4ad2262c
cxho <44372250+cxho@users.noreply.github.com>
2019-01-15 08:40:06 -0800

    while loop and strings

--- notes/Lizbeth Perez - Hangman.py
+++ notes/Lizbeth Perez - Hangman.py
@@ -1,16 +1,17 @@
 import random
+import string
+
 word_bank = ["i am starving", "Backpack", "clock", "sweaters", "pizza", "i like turtles", "tables", "camels",
              "Dora the Explorer", "did you do your homework?"]
 guesses_left = 8
 letters_guessed = []
 word = random.choice(word_bank)
-print(word)
 
 string1 = word
-list1 = list(string1)
-print(list1)
+word_list = list(string1)
+print(word_list)
 
-guess = input("Guess a letter.")
-letters_guessed.append(guess)
-print("you guessed the letter %s" % guess)
-8
+while guesses_left > 0:
+    guess = input("Guess a letter.")
+    letters_guessed.append(guess)
+    print("you guessed the letter %s" % guess)
