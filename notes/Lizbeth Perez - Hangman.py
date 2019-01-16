1959a09f1450a9e53b3414bd97655da08b96be98
cxho <44372250+cxho@users.noreply.github.com>
2019-01-16 08:38:48 -0800

    i dont even know

--- notes/Lizbeth Perez - Hangman.py
+++ notes/Lizbeth Perez - Hangman.py
@@ -15,3 +15,13 @@ while guesses_left > 0:
     guess = input("Guess a letter.")
     letters_guessed.append(guess)
     print("you guessed the letter %s" % guess)
+    for guess in letters_guessed:
+        if guess == list(string.ascii_letters):
+            current_index = letters_guessed.index(guess)
+            letters_guessed.pop(current_index)
+            letters_guessed.insert(current_index, "_")
+            print(letters_guessed)
+        else:
+            guesses_left -= 1
+            print(guesses_left)
+            print(letters_guessed)
