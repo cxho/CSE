5b35b52fe58d18ba759df61383adba113406e6ec
cxho <44372250+cxho@users.noreply.github.com>
2019-01-18 09:29:30 -0800

    change thing to a string

--- notes/Lizbeth Perez - Hangman.py
+++ notes/Lizbeth Perez - Hangman.py
@@ -1,5 +1,4 @@
 import random
-import string
 
 word_bank = ["i am starving", "Backpack", "clock", "sweaters", "pizza", "i like turtles", "tables", "camels",
              "Dora the Explorer", "did you do your homework?"]
@@ -9,7 +8,12 @@ word = random.choice(word_bank)
 
 string1 = word
 word_list = list(string1)
-print(word_list)
+
+for character in word_list:
+    current_index = word_list.index(character)
+    word_list.pop(current_index)
+    word_list.insert(current_index, "_")
+print("".join(word_list))
 
 while guesses_left > 0:
     if guess in word_list:
@@ -19,9 +23,3 @@ while guesses_left > 0:
     if guess not in word_list:
         print(guesses_left)
         guesses_left -= 1
-
-for character in word_list:
-    current_index = word_list.index(character)
-    word_list.pop(current_index)
-    word_list.insert(current_index, "_")
-print(word_list)
