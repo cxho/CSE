098e17911b3bfb8f1e1e667306e49edfe57737a3
cxho <44372250+cxho@users.noreply.github.com>
2019-01-31 08:42:39 -0800

    command

--- notes/Dictionary World Map - Lizbeth Perez.py
+++ notes/Dictionary World Map - Lizbeth Perez.py
@@ -8,7 +8,7 @@ world_map = {
         }
     },
     'PARKING_LOT': {
-        'NAME': "The Edison Parking Lot" ,
+        'NAME': "The Edison Parking Lot",
         'DESCRIPTION': "There are cars parked here. To the south is Mr. Wiebe's room.",
         'PATHS': {
             'SOUTH': "R19A"
@@ -16,3 +16,21 @@ world_map = {
     },
 
 }
+
+# Other Variables
+current_node = world_map["R19A"]
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+playing = True
+
+# Controller
+while playing:
+    print(current_node["NAME"])
+    command = input(">_")
+    if command.lower() in ["q", "quit", "exit"]:
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node["PATHS"][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
