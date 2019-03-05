a7a29d977c8bc79d6d6c8ea2e32dae5a04989a08
cxho <44372250+cxho@users.noreply.github.com>
2019-03-05 08:43:06 -0800

    items

--- notes/Items - Lizbeth Perez.py
+++ notes/Items - Lizbeth Perez.py
@@ -1,3 +1,51 @@
 class Item(object):
-    def __init__(self, packaging):
-        self.packaging
+    def __init__(self, name, packaging, labels, protection):
+        self.name = name
+        self.packaging = packaging
+        self.labels = labels
+        self.edible = True
+        self.protection = protection
+
+    def pick_up(self):
+        print("You picked up a type of food.")
+
+
+class Drinks(Item):
+    def __init__(self, name, packaging, labels, protection, temperature):
+        super(Drinks, self).__init__(name, packaging, labels, protection)
+        self.temperature = temperature
+        self.quantity = 100
+
+    def drink_liquid(self):
+        self.quantity -= 1
+        print("You drank the liquid")
+
+    def throw_liquid(self):
+        self.quantity = 0
+        print("You threw the drink")
+
+
+class HotDrinks(Drinks):
+    def __init__(self, name, packaging, labels, protection, temperature):
+        super(HotDrinks, self).__init__(name, packaging, labels, protection, temperature)
+        self.temperature = 75
+
+
+class Coffee(HotDrinks):
+    def __init__(self):
+        super(Coffee, self).__init__("Coffee", "Wrapper", "Ingredients", 20, 75)
+
+
+class Tea(HotDrinks):
+    def __init__(self):
+        super(Tea, self).__init__("Tea", "Tea bag", "How to prepare it", 20, 75)
+
+
+class HotChocolate(HotDrinks):
+    def __init__(self):
+        super(HotChocolate, self).__init__("Hot Chocolate", "none", "none", 20, 75)
+
+
+class Weapons(Item):
+    def __init__(self, name, ):
+
