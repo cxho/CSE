9ad4a5087bef1b5bf1de613c538bf35b68bde30e
cxho <44372250+cxho@users.noreply.github.com>
2019-05-01 09:42:12 -0700

    i multiplied numbers and it doesnt work :sunglasses:

--- notes/Validator - Lizbeth Perez.py
+++ notes/Validator - Lizbeth Perez.py
@@ -6,24 +6,21 @@ invalid = []
 
 def validate(num: str):
     if len(num) == 16:
-        last_num = int(num[15])
-        num -= last_num
-        num = num[:: - 1]
-        odd_nums = % 2 = 1
-
+        num = list(num)
+        num.pop(15)
+        return True
     else:
         invalid.append(num)
+    return False
 
 
-with open("Book1.csv", "r") as old_csv:
-    with open("MyNewFile.csv", "w") as new_csv:
-        reader = csv.reader(old_csv)
-        writer = csv.writer(new_csv)
-        print("processing...")
-        print("you will drown")
-        for row in reader:
-            old_number = row[0]  # String
-            first_num = int(old_number[0])
-            if validate(old_number):
-                writer.writerow(row)
-        print("ok")
+def reverse_num(num: str):
+    print(num[:: - 1])
+
+
+def multiply_odd_num(num: str):
+    if num[0: 15: 2]:
+        multiply = num[0: 15: 2]
+        multiply *= 2
+        if multiply > 9:
+            multiply -= 9
