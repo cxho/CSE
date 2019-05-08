065221fd2e5517aa63f1a6d899588bcbf1c8ced3
cxho <44372250+cxho@users.noreply.github.com>
2019-05-08 08:43:44 -0700

    i dont know what to do :camel:

--- notes/Sale Profits - Lizbeth Perez.py
+++ notes/Sale Profits - Lizbeth Perez.py
@@ -1,5 +1,11 @@
 import csv
 
 
-with open("SaleRecords.csv", "r"):
-
+with open("SalesRecords.csv", "r") as old_csv:
+    with open("BestProfits.csv", "w", newline="") as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("processing...")
+        for row in reader:
+            
+print("ok")
